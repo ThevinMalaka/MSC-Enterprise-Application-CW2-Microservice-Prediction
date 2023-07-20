a77a6edca3058d7f24ddb849bb4b38cd8ad32520
Thevin Malaka <thevin@orelit.com>
2023-07-20 23:20:07 +0530

    EF added

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//namespace predictionService.Data
+//{
+//	public class ApplicationDbContext
+//	{
+//		public ApplicationDbContext()
+//		{
+//		}
+//	}
+//}
+
+
+using predictionService;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+}
+
+
